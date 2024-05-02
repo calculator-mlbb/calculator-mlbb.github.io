62e4d9ac612e871693097b30edd592b855b6c004
calculator-mlbb <168771596+calculator-mlbb@users.noreply.github.com>
2024-05-03 01:26:25 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Converter from './component/Converter'
+
+const App = () => {
+
+  return (
+    <div>
+      <Converter />
+    </div>
+  ) 
+}
+
+export default App
